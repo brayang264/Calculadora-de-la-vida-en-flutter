92e61bf834a7c28798d056798b8477fb9d62a503
Brayan Gómez <brayan.gomez1@udea.edu.co>
2023-10-05 22:41:27 -0500

    Se empezo el desarollo en flutter

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:calculadora_de_la_vida/screens/mainScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp  extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "No se que va a ca",
+      home: mainScreen(),
+    );
+  }
+}
